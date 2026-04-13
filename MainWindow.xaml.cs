cab25a0c5fae496b4299b31e7d215a3935af8a1d
Malika <abdurahmanovamalika123@gmail.com>
2026-04-13 18:15:55 +0200

    Initial commit - WPF login UI

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace NimbusChat
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(EmailBox.Text) || string.IsNullOrWhiteSpace(PasswordBox.Password))
+            {
+                MessageBox.Show("Please enter email and password");
+            }
+            else
+            {
+                MessageBox.Show("Welcome!");
+            }
+        }
+    }
+}
