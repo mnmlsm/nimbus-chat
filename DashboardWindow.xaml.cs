1496c671ec438d20b0e21bbcbb1f78e838be54d0
Malika <abdurahmanovamalika123@gmail.com>
2026-07-01 13:37:04 +0200

    Improved dashboard design, weather card and profile overview

--- DashboardWindow.xaml.cs
+++ DashboardWindow.xaml.cs
@@ -44,11 +44,43 @@ namespace NimbusChat
             }
         }
 
+        public string Username => _currentUser.Username;
+
+        public string UserStatus =>
+            string.IsNullOrWhiteSpace(_currentUser.Status)
+                ? "Offline"
+                : _currentUser.Status;
+
+        public string FavoriteCity =>
+            string.IsNullOrWhiteSpace(_currentUser.FavoriteCity)
+                ? "Not selected"
+                : _currentUser.FavoriteCity;
+
+        public string WelcomeText =>
+            $"Welcome back, {_currentUser.Username}! 👋";
+
         public DashboardWindow(User currentUser)
         {
             InitializeComponent();
+
             _currentUser = currentUser;
+
+            // ОБЯЗАТЕЛЬНО!
             DataContext = this;
+
+            RefreshDashboard();
+        }
+
+        private void RefreshDashboard()
+        {
+            OnPropertyChanged(nameof(WelcomeText));
+            OnPropertyChanged(nameof(Username));
+            OnPropertyChanged(nameof(UserStatus));
+            OnPropertyChanged(nameof(FavoriteCity));
+
+            OnPropertyChanged(nameof(WeatherCity));
+            OnPropertyChanged(nameof(WeatherTemperature));
+            OnPropertyChanged(nameof(WeatherCondition));
         }
 
         private void OpenProfile_Click(object sender, RoutedEventArgs e)
@@ -58,7 +90,19 @@ namespace NimbusChat
                 Owner = this
             };
 
-            profileWindow.ShowDialog();
+            if (profileWindow.ShowDialog() == true)
+            {
+                var updatedUser = _userRepository.GetById(_currentUser.Id);
+
+                if (updatedUser != null)
+                {
+                    _currentUser.Username = updatedUser.Username;
+                    _currentUser.Status = updatedUser.Status;
+                    _currentUser.FavoriteCity = updatedUser.FavoriteCity;
+
+                    RefreshDashboard();
+                }
+            }
         }
 
         private void OpenWeather_Click(object sender, RoutedEventArgs e)
@@ -98,17 +142,17 @@ namespace NimbusChat
             Close();
         }
 
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            _userRepository.UpdateStatus(_currentUser.Id, "Offline");
+            base.OnClosing(e);
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        protected override void OnClosing(CancelEventArgs e)
-        {
-            _userRepository.UpdateStatus(_currentUser.Id, "Offline");
-            base.OnClosing(e);
         }
     }
 }
